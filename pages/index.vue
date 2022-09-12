8707bc3d444db82975fec99e268f89672a76fcf9
Frank Rousseau <frank.rousseau@free.fr>
2022-09-12 21:42:30 +0200

    wip

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,26 @@
 <template>
   <div>
     <Head>
-      <Title>{{ title }}</Title>
-      <Meta name="description" :content="title" />
+      <Title>
+        Kitsu - Platforme de collaboration pour studios d'animation
+      </Title>
+      <Meta
+        name="description"
+        content="Kitsu vous aide à livrer du contenu animé avec un petit budget. Répartissez les tâches, faites vos revues et gérez vos plans depuis partout."
+      />
     </Head>
-    <p>
-      <img src="/assets/kitsu.svg" class="w-24" />
-    </p>
+    <div class="">
+      <div>
+        <h1>Plateforme Collaborative Pour Studios d'Animation</h1>
+        <p>
+CGWire vous aide à livrer du contenu animé avec un petit budget. Répartissez
+les tâches, faites vos revues et gérez vos plans depuis partout.
+        </p>
+        <img src="/assets/kitsu.svg" class="w-24" />
+      </div>
+      <p class="text-center">
+        <img src="/assets/collaboration.svg" class="w-96" />
+      </p>
+    </div>
   </div>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Lato', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
