71da5d088e7d7480e2cfaf9a7704236b50e70dde
Frank Rousseau <frank.rousseau@free.fr>
2023-03-29 15:28:21 +0200

    [qa] Add not found page

---
+++ error.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="section content has-text-centered mt4">
+    <img src="~/assets/images/404.png" width="900">
+
+    <h1 class="mt4" v-if="error.statusCode === 404">Page not found</h1>
+    <h1 v-else>An error occurred - {{ error.statusCode }} </h1>
+    <NuxtLink to="/">Go back to home page</NuxtLink>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ['error'],
+    layout: 'error' // you can set a custom layout for the error page
+  }
+</script>
+
