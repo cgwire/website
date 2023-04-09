45f68d0a8951507c8042ff9734891a9d75e2978c
Frank Rousseau <frank.rousseau@free.fr>
2023-04-09 15:31:14 +0200

    [qa] remove nuxt folder

--- .nuxt/types/layouts.d.ts
+++
@@ -1,7 +0,0 @@
-import { ComputedRef, Ref } from 'vue'
-export type LayoutKey = "default"
-declare module "/home/frank/Projets/cgwire/website/node_modules/nuxt/dist/pages/runtime/composables" {
-  interface PageMeta {
-    layout?: false | LayoutKey | Ref<LayoutKey> | ComputedRef<LayoutKey>
-  }
-}
