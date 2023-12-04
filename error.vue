bd548f65211b2b887d03cb13450d49ded82f18f8
Frank Rousseau <frank.rousseau@free.fr>
2023-12-04 14:45:21 +0100

    [errors] Improve 404 page

--- error.vue
+++ error.vue
@@ -2,9 +2,13 @@
   <div class="section content has-text-centered mt4">
     <img src="~/assets/images/404.png" width="900">
 
-    <h1 class="mt4" v-if="error.statusCode === 404">Page not found</h1>
+    <h1
+      class="mt4"
+      v-if="error.statusCode === 404">
+        The page you were looking for could not be found...
+    </h1>
     <h1 v-else>An error occurred - {{ error.statusCode }} </h1>
-    <NuxtLink to="/">Go back to home page</NuxtLink>
+    <NuxtLink class="home-link" to="/">Go to home page</NuxtLink>
   </div>
 </template>
 
@@ -15,3 +19,14 @@
   }
 </script>
 
+<style scoped>
+h1 {
+  font-size: 1.8rem;
+  font-weight: bold;
+}
+
+.home-link {
+  font-weight: bold;
+  font-size: 1.8rem;
+}
+</style>
