8ceaab996a44f0702267e708e6524c59089a2d6b
Frank Rousseau <frank.rousseau@free.fr>
2024-02-05 00:31:06 +0100

    [feature] Add more references

--- pages/feature-films.vue
+++ pages/feature-films.vue
@@ -208,21 +208,66 @@
 
 <section class="section">
   <ul class="customers">
-    <li class="customers-item">
-      <a href="https://www.ankama.com/fr/animations">
-        <img src="~/assets/images/logo-ankama.png" alt="Ankama Animations">
-      </a>
-    </li>
-    <li class="customers-item">
-      <a href="https://www.autourdeminuit.com">
-        <img src="~/assets/images/logo-autour-de-minuit-invert.png" alt="Autour De Minuit">
-      </a>
-    </li>
-    <li class="customers-item">
-      <a href="https://www.supamonks.com/">
-        <img src="~/assets/images/studios/logo-supamonks-invert.png" alt="Supamonks">
-      </a>
-    </li>
+    <CustomerLogoBlock
+      link="https://www.ankama.com/fr/animations"
+      element-key="ankama"
+      name="Ankama Animations"
+    />
+    <CustomerLogoBlock
+      link="https://www.autourdeminuit.com/"
+      element-key="autour-de-minuit-invert"
+      name="Autour De Minuit"
+    />
+    <CustomerLogoBlock
+      link="https://les-fees-speciales.coop/"
+      element-key="les-fees-speciales-invert"
+      name="Les Fées Spéciales"
+    />
+    <CustomerLogoBlock
+      link="https://www.supamonks.com/"
+      element-key="supamonks-invert"
+      name="Supamonks"
+    />
+    <CustomerLogoBlock
+      link="http://www.onyx-productions.com/onyx/index.php"
+      element-key="onyx"
+      name="Onyx Films"
+    />
+    <CustomerLogoBlock
+      link="https://www.miyu.fr/"
+      element-key="miyu-invert"
+      name="Miyu"
+    />
+    <CustomerLogoBlock
+      link="https://www.uniko.com.es/"
+      element-key="uniko"
+      name="Uniko"
+    />
+    <CustomerLogoBlock
+      link="https://www.3point0studio.com/"
+      element-key="30studio"
+      name="3.0 Studio"
+    />
+    <CustomerLogoBlock
+      link="https://lesvalseurs.com/"
+      element-key="lesvalseurs"
+      name="Les Valseurs"
+    />
+    <CustomerLogoBlock
+      link="https://www.parmilesluciolesfilms.fr/"
+      element-key="parmi-les-lucioles"
+      name="Parmi les lucioles"
+    />
+    <CustomerLogoBlock
+      link="https://traumhaus-studios.de/"
+      element-key="traumhaus-invert"
+      name="Traumhaus"
+    />
+    <CustomerLogoBlock
+      link="https://makutavfx.com/"
+      element-key="makuta"
+      name="Makuta"
+    />
   </ul>
 </section>
 
