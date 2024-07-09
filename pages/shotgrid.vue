4d2d97bd60b1e06e18bca7aa0df40d0bc7e04c5a
andrewlawrence0 <andrew.lawrence102@gmail.com>
2024-07-09 11:38:44 +0900

    Proof read *ShotGrid* document

--- pages/shotgrid.vue
+++ pages/shotgrid.vue
@@ -14,79 +14,66 @@
     <h3>Target audience</h3>
 
     <p>
-    The target audience of Shotgrid is big facilities: AutoCAD customers and
-    large studios like ILM, Weta, or Disney.
+    The target audience of Shotgrid includes large VFX facilities, such as ILM, Weta, and Disney.
     </p>
     <p>
-    The Kitsu audience are animation and VFX studios with distributed teams
-    and productions involving several studios.
+    The target audience for Kitsu includes animation and VFX studios with distributed teams and productions involving multiple studios.
     </p>
     <p>
-    Features and roadmaps are designed accordingly to these two different goals.
-    That's why you should consider this aspect before choosing your
-    reference solution.
+    Our features and roadmaps are tailored to meet these distinct needs. Therefore, it’s important to consider this aspect when choosing your production management solution.
     </p>
 
     <h3>Resource needed</h3>
 
     <p>
-    Shotgrid is a toolbox. It doesn't work right out of the box. So, to start
-    with Shotgrid, consider hiring several people dedicated to
-    designing and configuring UIs in Shotgrid. Then you have to plug the whole
-    into your pipeline.
-    You will end up with something tailor-made but at the
-    price of a lot of
-    effort. These developments will require a lot of maintenance in the
-    future. Last, you will need training sessions that present
-    your version of Shotgrid.
+    ShotGrid is an expansive toolbox. To get started with ShotGrid, you must hire 
+    several people dedicated to customizing UIs, and integrating it into your pipeline. 
+    You will end up with something tailor-made, but it will require a lot of effort. 
+    These developments may also need significant maintenance in the future. 
+    Lastly, you will need internal training sessions for your customized version of ShotGrid.
     </p>
 
     <p>
-    Kitsu works
-    great right out of the box. All UIs are designed with ease of use in mind.
-    It's easy to start with it, and your users are autonomous. Nevertheless, you
-    will still need a TD to plug your pipeline into Kitsu and exploit
-    its database. Although it provides flexibility to define different
-    workflows, Kitsu may not fit with very particular projects too.
+    In contrast, Kitsu works great right out of the box. All UIs are designed with ease of use in mind, 
+    making it easy to get started. Our dedicated support team is available to assist with setup if needed.
+    However, to fully integrate Kitsu with your pipeline and take advantage of its database, you may need a pipeline TD. 
+    While Kitsu provides flexibility to define different workflows, it may not be suitable for projects with highly specialized pipelines.
     </p>
 
 
-    <h3>Simplicity vs Exhaustivity</h3>
+    <h3>Simplicity vs Comprehensiveness</h3>
 
     <p>
-    Shotgrid is thought to be able to work on any kind of project at fine-grain
-    level. It's great to deal with any situation but it comes with a lot
-    of complexity. It may be too hard for your teams to get the full benefits.
+    ShotGrid is designed to work on any project at a fine-grain level. 
+    It's great at adapting to any situation, but it comes with a lot of complexity. As a result, 
+    the added time taken to learn and customize the system may counter the benefits gained from it. 
+    Moreover, projects that require this level of specificity are rare.
     </p>
 
     <p>
-    Kitsu focuses on animation and VFX projects. From our discussions with
-    hundreds of studios, we made strong choices to propose
-    something efficient but flexible enough to adapt to most situations.
-    Our goal is to ensure that everyone in the studio can use Kitsu from
-    day 1.
+    Kitsu focuses on animation and VFX projects. 
+    Through close collaboration with hundreds of studios, we have designed a system that is efficient yet flexible enough to adapt to most situations. 
+    Our goal is to ensure that everyone in the studio can use Kitsu from day one. 
+    While there may be some very specialized use cases that aren't suitable, you can be confident that everyone in the studio will be comfortable with Kitsu.
     </p>
 
     <h3>Security</h3>
 
     <p>
-    Shotgrid qualifies for many security certifications and provides on-premise
-    installations (with a high extra cost). Its code is proprietary,
-    so there is no certainty that your data will stay private.
+    ShotGrid qualifies for many security certifications and although it offers an on-premise installations, there is a lengthy approval process and incredibly high additional cost.
+    Its code is also proprietary, so there is no guarntee that features or functionality might change or break in the future.
     </p>
 
     <p>
-    Kitsu is open-source and can be fully audited. It can be installed
-    on-premise with very little extra-cost. Its open-source aspect ensures
-    that your data stay private and secure.
+    Kitsu is open-source and can be fully audited and installed on-premise with very little extra cost. 
+    We ensure that your data remains private and secure. 
+    We take security very seriously and, while still waiting for certifications, we apply all best practices to keep your data safe.
     </p>
 
     <p>
-    CGWire engineers have a strong track record in cloud hosting and apply all
-    the best practices to ensure the security of your data (HTTPS, strict
-    password management politic, strong SSH key management, background checks,
-    etc.). It's a matter of time before we to qualify for the needed security
-    certifications.
+    CGWire engineers have a strong track record in cloud hosting and apply all protocols to ensure the security of your data (HTTPS, 
+    strict password management policies, strong SSH key management, background checks, etc.). 
+    It is only a matter of time before we qualify for the necessary security certifications.
     </p>
 
     <h3>Pricing</h3>
@@ -96,10 +83,7 @@
     </p>
 
     <p>
-    But we provide more services. Our support is
-    recognized to be much more reactive than the one from Autodesk, and Kitsu
-    performs better when used with animation or VFX productions. Installations
-    can be more customised too.
+    However, our support is recognized for being much more responsive than Autodesk's, and we provide additional assistance for customized installations.
     </p>
 
     <p>
@@ -109,13 +93,11 @@
 
     <h3>Documentation</h3>
     <p>
-    Shotgrid provides user documentation and a doc about its Python client.
+    Shotgrid provides user documentation for it's app and Python client.
     </p>
     <p>
-    Kitsu provides an easy to follow the documentation, a search engine, tutorial
-    videos.
-    Its Python and its REST API are fully documented. It's much easier for
-    everyone to work with Kitsu.
+    Kitsu provides easy-to-follow documentation, a search engine, and tutorial videos. 
+    Its Python client and REST API are fully documented, making it much easier for everyone to work with Kitsu.
     </p>
   </section>
 
